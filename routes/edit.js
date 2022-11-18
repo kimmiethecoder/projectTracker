bc49afb307d8a71105bb2d8e564314af527fb592
Kimberly Rodriguez <kimmiethecoder@gmail.com>
2022-11-18 10:35:47 -0500

    middleware errors solved-removed

--- routes/edit.js
+++ routes/edit.js
@@ -1,10 +1,11 @@
 //This route handles editing/deleting items, renders edit page
 
-const express = require('express')
-const router = express.Router()
-const editController = require('../controllers/edit')
+const express = require('express');
+const router = express.Router();
+const editController = require('../controllers/edit');
+const authController = require("../controllers/auth");
 
-router.get('/remove/:id', editController.deleteTicket)
-router.get('/:id', editController.updateTicket)
+router.get('/remove/:id', editController.deleteTicket);
+router.get('/:id', editController.updateTicket);
 
 module.exports = router
--- routes/home.js
+++ routes/home.js
@@ -4,16 +4,11 @@
 const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
 
 router.get('/', homeController.getIndex);
-router.get("/profile", ensureAuth, homeController.getProfile);
+
 router.post('/new', homeController.createTicket);
-router.get("/login", authController.getLogin);
-router.post("/login", authController.postLogin);
-router.get("/logout", authController.logout);
-router.get("/signup", authController.getSignup);
-router.post("/signup", authController.postSignup);
 
 
 module.exports = router
